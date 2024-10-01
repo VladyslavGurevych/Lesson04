3eb21e84f3b9dbcb0f7833a56b76857a4c44d2df
Vladyslav Hurevych <gurik1989@ukr.net>
2024-10-01 10:27:30 +0100

    Delete alternative variant

--- Lesson4/src/app/Lesson4.java
+++ Lesson4/src/app/Lesson4.java
@@ -13,12 +13,6 @@ public class Lesson4 {
                 "sales by day is EUR %.2f.\n",
                 orderNumber, productType, numberOfDays, priceForItem*saleItemsPerDay*numberOfDays, amountPerDay);
 
-        /* Alternative with division inside f-string without variable amountPerDay
-        System.out.printf("Product No %d%s\n" + "total sales for %d days is EUR %.2f,\n" +
-                        "sales by day is EUR %.2f.\n",
-                orderNumber, productType, numberOfDays, amount, amount / numberOfDays);
-        */
-
         orderNumber = 2;
         numberOfDays = 7;
         amount = 10486.85;
@@ -31,10 +25,5 @@ public class Lesson4 {
                         "sales by day is EUR %.2f.\n",
                 orderNumber, productType, numberOfDays, priceForItem*saleItemsPerDay*numberOfDays, amountPerDay);
 
-        /* Alternative with division inside f-string without variable amountPerDay
-        System.out.printf("Product No %d%s\n" + "total sales for %d days is EUR %.2f,\n" +
-                        "sales by day is EUR %.2f.\n",
-                orderNumber, productType, numberOfDays, amount, amount / numberOfDays);
-        */
     }
 }
