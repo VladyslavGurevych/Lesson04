81fa856dd4b83862db49873805fc851bc2ae36f6
Vladyslav Hurevych <gurik1989@ukr.net>
2024-10-01 10:00:37 +0100

    Change of calculating method

--- Lesson4/src/app/Lesson4.java
+++ Lesson4/src/app/Lesson4.java
@@ -4,12 +4,14 @@ public class Lesson4 {
         int orderNumber = 1;
         int numberOfDays = 5;
         double amount = 12153.41;
+        int saleItemsPerDay = 8;
         double amountPerDay = amount / numberOfDays;
+        double priceForItem = amountPerDay/saleItemsPerDay;
         String productType = ": smartphone,";
 
         System.out.printf("Product No %d%s\n" + "total sales for %d days is EUR %.2f,\n" +
                 "sales by day is EUR %.2f.\n",
-                orderNumber, productType, numberOfDays, amount, amountPerDay);
+                orderNumber, productType, numberOfDays, priceForItem*saleItemsPerDay*numberOfDays, amountPerDay);
 
         /* Alternative with division inside f-string without variable amountPerDay
         System.out.printf("Product No %d%s\n" + "total sales for %d days is EUR %.2f,\n" +
@@ -20,12 +22,14 @@ public class Lesson4 {
         orderNumber = 2;
         numberOfDays = 7;
         amount = 10486.85;
+        saleItemsPerDay = 3;
         amountPerDay = amount / numberOfDays;
+        priceForItem = amountPerDay/saleItemsPerDay;
         productType = ": laptop,";
 
         System.out.printf("Product No %d%s\n" + "total sales for %d days is EUR %.2f,\n" +
                         "sales by day is EUR %.2f.\n",
-                orderNumber, productType, numberOfDays, amount, amountPerDay);
+                orderNumber, productType, numberOfDays, priceForItem*saleItemsPerDay*numberOfDays, amountPerDay);
 
         /* Alternative with division inside f-string without variable amountPerDay
         System.out.printf("Product No %d%s\n" + "total sales for %d days is EUR %.2f,\n" +
